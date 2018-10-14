2e7dfa53826d3d1bed33b0bd3eb59e74ad213e7f
Sovent <vmargelov@gmail.com>
2018-10-14 15:55:59 +0300

    leaguegram implemented

---
+++ ThirdLesson/LeagueGram/Infrastructure/InMemoryChatRepository.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using LeagueGram.Domain;
+using LeagueGram.Domain.Exception;
+
+namespace LeagueGram.Infrastructure
+{
+  public class InMemoryChatRepository : IChatRepository
+  {
+    public IChat LoadChat(Guid chatId)
+    {
+      if (!_chats.ContainsKey(chatId))
+      {
+        throw new ChatNotFoundException(chatId);
+      }
+
+      return _chats[chatId];
+    }
+
+    public void SaveChat(IChat chat)
+    {
+      _chats[chat.Id] = chat;
+    }
+
+    private readonly Dictionary<Guid, IChat> _chats = new Dictionary<Guid, IChat>();
+  }
+}
