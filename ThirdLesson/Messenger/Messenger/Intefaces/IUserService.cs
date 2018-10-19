d543044d09eef98326771c90722830e71e6267bc
LavanderSea <tanya.smith1712@gmail.com>
2018-10-19 21:39:01 +0300

    completed task

---
+++ ThirdLesson/Messenger/Messenger/Intefaces/IUserService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Messenger
+{
+    public interface IUserService
+    {
+        void Identification(Guid user_id);
+
+        void ChangeMessage(string changed_message, Guid message_id, IChat chat);
+        void RemoveMessage(Guid message_id, IChat chat);
+        void SendMessage(IMessage message, IChat chat);
+        List<IMessage> GetMessages(IChat chat);
+        IMessage GetLastMessages(IChat chat);
+    }
+}
---
+++ ThirdLesson/Messenger/Messenger/Objects/User.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Messenger
+{
+    public class User : IUser
+    {
+        public User(string nickname, Guid id)
+        {
+            this.nickname = nickname ?? throw new ArgumentNullException(nameof(nickname));
+            _id = id;
+        }
+
+        public string nickname { private set; get; }
+        public Guid _id { get; }
+
+        public override bool Equals(object obj)
+        {
+            var user = obj as User;
+            return user != null &&
+                   nickname.Equals(user.nickname) &&
+                   _id.Equals(user._id);
+        }
+
+        public override int GetHashCode()
+        {
+            var hashCode = -647874936;
+            hashCode = hashCode * -1521134295 + EqualityComparer<string>.Default.GetHashCode(nickname);
+            hashCode = hashCode * -1521134295 + EqualityComparer<Guid>.Default.GetHashCode(_id);
+            return hashCode;
+        }
+    }
+}
