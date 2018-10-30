90d9aeedd5252336b45836e98907605eedaa1754
LavanderSea <tanya.smith1712@gmail.com>
2018-10-30 18:43:47 +0300

    third test

--- ForthLesson/Leo-sprint/SessionTest/UserTest.cs
+++ ForthLesson/Leo-sprint/SessionTest/UserTest.cs
@@ -31,5 +31,17 @@ namespace SessionTest
             var expected_list = new List<Word> { new Word("eng1", "rus1", 0) };
             CollectionAssert.ReferenceEquals(list, expected_list);
         }
+        [TestMethod]
+        public void SecondCheckRightAnswers_GetEmptyListOfWords()
+        {
+            var words_in_process = new List<Word> { new Word("eng", "rus", 0), new Word("eng1", "rus1", 0) };
+            var test_words = new List<Word> { new Word("eng", "rus", 0), new Word("eng1", "rus2", 0) };
+            var test_user = new User(string.Empty, Guid.Empty, new List<Word>(), words_in_process);
+            var answers = new bool[] { true, false };
+
+            var list = test_user.CheckAnswers(answers, words_in_process.ToArray());
+            var expected_list = new List<Word> ();
+            CollectionAssert.ReferenceEquals(list, expected_list);
+        }
     }
 }
