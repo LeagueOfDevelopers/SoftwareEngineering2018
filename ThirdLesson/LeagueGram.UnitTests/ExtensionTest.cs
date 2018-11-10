1bf757c25fc0c7ef061673a085495c523d48c2b5
Vitaly Margelov <Sovent@users.noreply.github.com>
2018-11-10 16:09:27 +0300

    changed name

--- ThirdLesson/LeagueGram.UnitTests/ExtensionTest.cs
+++ ThirdLesson/LeagueGram.UnitTests/ExtensionTest.cs
@@ -9,7 +9,7 @@ namespace LeagueGram.UnitTests
 		[TestMethod]
 		public void DecapitalizeString_FirstCharIsInLowerCase()
 		{
-			var stringToDecapitalize = "Vasya";
+			var stringToDecapitalize = "Zhenya";
 						
 			var result = stringToDecapitalize.Decapitalize();
 
